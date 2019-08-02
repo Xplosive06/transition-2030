5f68c7eb4b48c07530c00d1340bec0415e28495f
Mike <nottamike92@gmail.com>
2019-08-02 16:02:41 +0200

    change titles dinamically with helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,12 @@
 <?php
+
+if (! function_exists('page_title')){
+    function page_title($title) {
+        $base_title = 'Transition 2030 - Le site';
+        if($title === '') {
+            return $base_title;
+        } else {
+            return $title . ' | ' . $base_title;
+        }
+    }
+}
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
 
-    <title>{{page_title($title ?? '')}} Transition 2030 - Le site</title>
+    <title>{{page_title($title ?? '')}}</title>
 
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
