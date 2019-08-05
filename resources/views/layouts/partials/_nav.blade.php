90ebb61c078568788bfc66da231ff1df08e1422a
Mike <nottamike92@gmail.com>
2019-08-05 20:39:49 +0200

    add authentication for login and register menu

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -28,11 +28,33 @@
                         <li><a href="#">One more separated link</a></li>
                     </ul>
                 </li>
-                <li class="{{ set_active_route('contacts.create') }}"><a href="{{ route('contacts.create') }}">Contact</a></li>
+                <li class="{{ set_active_route('contacts.create') }}"><a
+                        href="{{ route('contacts.create') }}">Contact</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#login">Se connecter</a></li>
-                <li><a href="#register">S'enregistrer</a></li>
+                @guest
+                    <li><a href="{{ route('login') }}">Se connecter</a></li>
+                    <li><a href="{{ route('register') }}">S'enregistrer</a></li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
             </ul>
         </div><!--/.nav-collapse -->
     </div>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/about', 'PagesController@about')->name('about');
 Route::get('/contact', 'ContactsController@create')->name('contacts.create');
 
 Route::post('/contact', 'ContactsController@store')->name('contacts.store');
+
+Auth::routes(['verify' => true]);
+
+/*Route::get('/home', 'HomeController@index')->name('home');*/
