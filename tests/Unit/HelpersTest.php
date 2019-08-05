2ee62e534cb86f21562f88ab0afa344d94047fbb
Mike <nottamike92@gmail.com>
2019-08-05 15:16:27 +0200

    add scope twoMonthsOld

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -14,9 +14,18 @@ class HelpersTest extends TestCase
         $this->assertEquals("Transition-2030 - Le site", page_title(''));
     }
 
+    /** @test */
     public function page_title_should_return_the_correct_title_if_title_is_provided()
     {
         $this->assertEquals("About | Transition-2030 - Le site", page_title('About'));
         $this->assertEquals("Home | Transition-2030 - Le site", page_title('Home'));
+    }
+
+    /** @test */
+    public function set_active_route_should_return_the_correct_class_based_on_a_given_route()
+    {
+        $this->get(route('root_path'));
+        $this->assertEquals('active', set_active_route('root_path'));
+        $this->assertEquals('', set_active_route('about_path'));
     }
 }
